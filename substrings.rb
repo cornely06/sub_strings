82e0c2566641422eb530fc7c3afec69af21f6313
Raymond Cornely <cornely06@gmail.com>
2021-03-27 17:44:15 -0400

    Add method to find substring tally in provided string

--- substrings.rb
+++ substrings.rb
@@ -1,1 +1,10 @@
+def substrings(string, words)
+    tally = Hash.new(0)
+    words.map do |word|
+      if string.include? word
+        tally[word] += 1
+      end
+    end
+   return tally
+end
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
